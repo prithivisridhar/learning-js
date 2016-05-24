4f79615eebbdebf1ba81dd1e04723a911cc2b982
Niranjan Vijay Awati <kneerunjun@gmail.com>
2016-05-24 14:47:33 +0530

    modified variables demo

--- learning-js/variablesdemo.js
+++ learning-js/variablesdemo.js
@@ -1,1 +1,62 @@
-﻿
+﻿console.log("hello world!!");
+
+var myName = "niranjan awati";
+console.log(typeof myName);
+myName = 2334;
+console.log(typeof myName);
+
+
+console.log(myName);
+
+var empno = 41993;
+console.log(empno);
+
+var precision = 2.3;
+console.log(precision);
+console.log(typeof precision);
+
+var position;
+console.log(position);
+console.log(typeof position);
+console.log(position === undefined);
+
+//JSON style of making new objects
+var user = {
+    name: "niranjan awati",
+    empno: 41993,
+    location: "pune",
+    profile: "lecturer"
+};
+
+console.log(typeof user);
+
+var locations = ["Pune", "Mysore", "Bangalore", "Chandigarh", "Hyderbad"];
+console.log(typeof locations);
+
+var nullObject = null;
+console.log(typeof nullObject);
+
+var myStatus = false;
+console.log(typeof myStatus);
+
+//expression == delegate, lambda expression, callback
+var calcWeight = function (gravity, planet) {
+
+};
+console.log(typeof calcWeight);
+
+
+/*
+fsdkfsdkljfjksdfhjkdshfkjsd
+fdsjfhdsjkfhkdsfhkdjsfhdjks
+fsdfjkdsfhdksjfhdsljfdsljfsd
+fldsjhfdfhsdkjfhd,j
+*/
+
+//function, c`tor function
+function Dothis(param1, param2) {
+    return param1 + param2;
+}
+console.log(typeof Dothis);
+console.log(Dothis(10, 20));
+console.log(Dothis(10));
