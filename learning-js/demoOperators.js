3d728833f7fd67f92959343406b0657b6c201a6c
Niranjan Vijay Awati <kneerunjun@gmail.com>
2016-05-25 16:13:03 +0530

    reference type demo

--- learning-js/demoOperators.js
+++ learning-js/demoOperators.js
@@ -31,3 +31,50 @@ console.log(10 + user);
 console.log(10 * 10);
 console.log(10 / 10);
 console.log(10 / 0);
+
+console.log("this is where we look at the unary ops");
+var baseValue = 9;
+console.log(baseValue++);
+console.log(++baseValue);
+
+var result = baseValue++ * 10;
+console.log(result);
+
+result = ++baseValue * 10;
+console.log(result);
+
+
+console.log("we are here to test the boolean values");
+console.log(true && true);
+console.log(false && true);
+console.log(false && false);
+var obj = { attr: "niranjan" };
+console.log(obj && true);
+console.log(obj && false);
+console.log(obj && 99);
+console.log(true && obj);
+console.log(false && obj);
+
+console.log("trying for the or ops ");
+console.log(true || true);
+console.log(false || true);
+console.log(false || false);
+console.log(obj || true);
+console.log(obj || false);
+console.log(obj || 99);
+console.log(true || obj);
+console.log(false || obj);
+
+
+console.log("equality operators");
+console.log(true == true);
+console.log(false == false);
+console.log(42 == 42);
+console.log(42 == "42");
+
+
+var obj = { valueOf: function () { return 42; } };
+console.log(42 == obj);
+
+console.log(42 === "42");
+console.log(42 === obj);
---
+++ learning-js/referenceTypes.js
@@ -0,0 +1,42 @@
+﻿//this is what is called the JSON notation for objects
+var user = {
+    name: "niranjan",
+    location:"Pune"
+}
+
+function AlterUser(u) {
+    u.name = "name_altered";
+}
+AlterUser(user);
+console.log(user.name);
+
+
+var locations = ["Pune", "Bangalore", "Mysore", "Chandigarh"];
+console.log(locations instanceof Array);
+
+var remainingLocations = ["Mumbai", "Delhi"];
+var mergerLocations = locations.concat(remainingLocations);
+console.log(mergerLocations);
+
+var mysore = mergerLocations.filter(function (el, idx) {
+    return el == "Mysore";
+});
+console.log(mysore);
+
+var users = [
+    { name: "fsdf", location: "Pune" },
+    { name: "fsdf", location: "Pune" },
+    { name: "fsdf", location: "Mysore" },
+    { name: "fsdf", location: "Delhi" },
+    { name: "fsdf", location: "Bangalore" },
+    { name: "fsdf", location: "Pune" },
+    { name: "fsdf", location: "Pune" },
+    { name: "fsdf", location: "Pune" },
+    { name: "fsdf", location: "Mysore" },
+    { name: "fsdf", location: "Mysore" },
+
+];
+var mysoreUsers = users.filter(function (el, indx) {
+    return el.location == "Mysore";
+});
+console.table(mysoreUsers);
