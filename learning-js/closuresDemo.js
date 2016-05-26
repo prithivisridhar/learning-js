130e1eb1839e6ba92730c3308421e4242e7d24e2
Niranjan Vijay Awati <kneerunjun@gmail.com>
2016-05-26 15:47:19 +0530

    demo for closures

---
+++ learning-js/closuresDemo.js
@@ -0,0 +1,12 @@
+﻿var filter = function () {
+    var state = 500;
+    return function () {
+        return state += 10;
+    }
+}
+
+var flt = filter();
+console.log(flt);
+console.log(flt());
+console.log(flt());
+console.log(flt());
